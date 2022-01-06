1c52626e33d8c596f3278f6c641670dc1534ccfe
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-01-06 08:50:49 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,8 +6,7 @@ function NavBar(){
   const{result } = useContext(AppContext)
     return(
         <>
-      <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-    
+    <nav className="navbar navbar-expand-lg navbar-light bg-block">    
   <a className="navbar-brand" href="a" style={{color:"white" , fontWeight:"bolder" , fontSize:"1.5rem"}}>Shortner</a>
   <p className="profile">{result.name}</p>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
