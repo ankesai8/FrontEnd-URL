06e01299801a18a1b019aacefeb2a75b175efc81
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-01-06 09:01:50 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,6 +8,20 @@ function NavBar(){
         <>
     <nav className="navbar navbar-expand-lg navbar-light bg-block">    
   <a className="navbar-brand" href="a" style={{color:"black" , fontWeight:"bolder" , fontSize:"1.5rem"}}>Shortner</a>
+
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+      <div className="icons">
+    <i class="fas fa-user fa-2x"></i>
+    <div className="logSign"></div>
+    </div>
+    </ul>
+  </div>
+ 
  
 </nav>
         </>
