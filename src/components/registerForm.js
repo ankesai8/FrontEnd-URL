255bc50b09bdd83cb9249eb56f904ebd761aa8f5
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-01-06 09:37:59 +0530

    Update registerForm.js

--- src/components/registerForm.js
+++ src/components/registerForm.js
@@ -13,7 +13,7 @@ function RegisterForm() {
 //   addeing new user data to database
   async function registerNew() {
     try {
-      const response = await axios.post("https://suraj-url-backend.herokuapp.com/user/register", {
+      const response = await axios.post("https://url-short-ner-6.herokuapp.com//user/register", {
         email: name,
         password: password,
         name: firstName,
@@ -51,49 +51,17 @@ function RegisterForm() {
       <div className="containers">
         <div className="form">
           <form onSubmit={handleSubmit}>
-            <div className="titles">
-              <p className="welcome">Register</p>
-            </div>
+            <div className="titles"><p className="welcome">Registration</p></div>
 
-            <input
-              className="form-control text"
-              type="text"
-              value={firstName}
-              name="firstName"
-              placeholder="Enter your first name"
-              required
-              onChange={handleChange}
-            ></input>
-            <br />
+           <input className="form-control text" type="text" value={firstName} name="firstName" placeholder="Enter your Name" required onChange={handleChange} ></input>
+           <br />
+           <input className="form-control text" type="email" value={name} name="userName" placeholder="Enter your email" required onChange={handleChange} ></input> 
+           <br /> 
+           <input className="form-control text" type="password" value={password} name="password" placeholder="Enter password" required onChange={handleChange} ></input>
+           <br />  
+           <button type="submit" className="btn-lg btn-block btn btn-primary btns" >Register</button>
 
-            <input
-              className="form-control text"
-              type="email"
-              value={name}
-              name="userName"
-              placeholder="Register your email"
-              required
-              onChange={handleChange}
-            ></input>
-            <br />
-
-            <input
-              className="form-control text"
-              type="password"
-              value={password}
-              name="password"
-              placeholder="Create password"
-              onChange={handleChange}
-              required
-            ></input>
-            <br />
-
-            <button
-              type="submit"
-              className="btn-lg btn-block btn btn-primary btns"
-            >
-              Register
-            </button>
+            
           </form>
         </div>
       </div>
