bdcb7c5beb7f3b08d14b513ea7d071e35b5abda2
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-01-06 09:19:27 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,7 @@ function NavBar(){
         <>
         <nav className="navbar navbar-expand-lg navbar-light bg-block">
   <a className="navbar-brand" href="a" style={{color:"black" , fontWeight:"bolder" , fontSize:"2.4rem"}}>URL Shortner</a>
-   <p>Shorten ur links using MERN
+   <p>Shorten ur links using MERN</p>
   <p className="profile">{result.name}</p>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
